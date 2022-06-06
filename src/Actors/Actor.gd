2a17fb682a01bf2232858a25ed58015519d5ab97
Shashwat Singh <70637141+SXH7@users.noreply.github.com>
2022-06-06 13:08:58 +0530

    removed gravity

--- src/Actors/Actor.gd
+++ src/Actors/Actor.gd
@@ -2,10 +2,5 @@ extends KinematicBody2D
 class_name Actor
 
 export var speed: = Vector2(500, 1000)
-export var gravity: = 1000.0
-
+export var gravity: = 30000.0
 var velocity: = Vector2.ZERO
-
-func _physics_process(delta):
-	velocity.y += gravity*delta
-	move_and_slide(velocity)
